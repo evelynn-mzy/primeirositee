f941aaed1732fbdcff2c1f711bfced472588a40d
evelynn-mzy <evelyn.galan@escola.pr.gov.br>
2026-06-25 09:57:28 -0300

    siteeee

---
+++ script.js
@@ -0,0 +1,8 @@
+const curtidas=document.getElementById("curtidas") //pega botão de gostei pelo id
+curtidas.addEventListener("click",aumentarcurtidas)//quando clicar chama a função
+
+//função de aumentar curtidas
+function aumentarcurtidas(){ 
+    let quantidade =document.querySelector("span")
+    quantidade.textContent++;0
+}
